caf29fd075efac44c4bc60af981b26121fe83c65
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2018-05-02 02:35:11 +0600

    Updated some file

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 2000,
   mongoose = require('mongoose'),
   Task = require('./model/model'), //created model loading here
   bodyParser = require('body-parser');
--- routes/routes.js
+++ routes/routes.js
@@ -11,6 +11,7 @@ module.exports = function(app) {
       
     app.route('/members/name')
       .get(todoList.list_all_tasks)
+      .post(todoList.create_a_task)
       .put(todoList.update_a_task)
       .delete(todoList.delete_a_task);
   };
