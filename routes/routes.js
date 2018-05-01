0d09034659b1e660c45791017b5bc97dab14ef09
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2018-05-02 02:19:40 +0600

    ReSTful-Api for project database

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+//'use strict';
+module.exports = function(app) {
+    var todoList = require('../controller/controller');
+  
+    // todoList Routes
+    app.route('/members')
+      .get(todoList.list_all_tasks)
+      .post(todoList.create_a_task);
+  
+  
+      
+    app.route('/members/name')
+      .get(todoList.list_all_tasks)
+      .put(todoList.update_a_task)
+      .delete(todoList.delete_a_task);
+  };
+  
